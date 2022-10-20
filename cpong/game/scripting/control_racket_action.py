e9b6e8a718c4b72ff4b1aaf4576dfb6ad6e1d4b0
BobHathaway2 <rlhathaway56@yahoo.com>
2022-10-19 20:42:35 -0600

    rackets moving independently

--- cpong/game/scripting/control_racket_action.py
+++ cpong/game/scripting/control_racket_action.py
@@ -10,10 +10,18 @@ class ControlRacketAction(Action):
     def execute(self, cast, script, callback):
         rackets = []
         rackets = cast.get_actors(RACKET_GROUP)
-        for racket in rackets:
-            if self._keyboard_service.is_key_down(LEFT): 
-                racket.swing_left()
-            elif self._keyboard_service.is_key_down(RIGHT): 
-                racket.swing_right()  
-            else: 
-                racket.stop_moving()        
+        for i, racket in enumerate(rackets):
+            if i == 0:
+                if self._keyboard_service.is_key_down('s'): 
+                    racket.swing_left()
+                elif self._keyboard_service.is_key_down('f'): 
+                    racket.swing_right()  
+                else: 
+                    racket.stop_moving()
+            else:
+                if self._keyboard_service.is_key_down('j'): 
+                    racket.swing_left()
+                elif self._keyboard_service.is_key_down('l'): 
+                    racket.swing_right()  
+                else: 
+                    racket.stop_moving()
